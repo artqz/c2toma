925dabee0b525cc9376a3fe0191bb96dbf8db287
artqz <djoctuk@yandex.ru>
2023-05-25 16:40:42 +0500

    .

--- import/effects.ts
+++ import/effects.ts
@@ -1,7 +1,9 @@
-import { SkillEnffect, loadSkillDataC4 } from '../datapack/c4/skilldata';
+import { SkillEnffect, SkillOperateCond, loadSkillDataC4 } from '../datapack/c4/skilldata';
 import { Skill } from '../result/types';
 import { saveFile } from '../utils/Fs';
 const effectNameMap = new Map()
+const operateCondNameMap  = new Map()
+const map = new Map<string, {skillName: string, effect: string, operateCond: string}>()
 
 export function loadEffects (deps: {
   skills: Map<string, Skill>
@@ -11,10 +13,12 @@ export function loadEffects (deps: {
     const skillC4 = skillC4ByName.get(skill.skillName)
     if (skillC4) {
       effectReader({effect: skillC4.effect, skillName: skill.skillName})
+      operateCondReader({operateCond: skillC4.operate_cond, skillName: skill.skillName})
     }
   }
   //delete
-  saveFile("result/data/effects.json", JSON.stringify(Array.from(effectNameMap.values()), null, 2))
+  saveFile("result/data/effects.json", JSON.stringify(Array.from(map.values()), null, 2))
+  //  saveFile("result/data/operateConds.json", JSON.stringify(Array.from(operateCondNameMap.values()), null, 2))
   //
   
   return deps.skills
@@ -26,21 +30,13 @@ export function effectReader(deps: {
   skillName: string;
 }) {
   deps.effect.$?.map((e) => {
-    switch (e.$[0]) {
-      case "i_p_attack_over_hit":
-        // console.log(`Power: ${e.$[1]}`);
-        break;
-      case "p_speed":
-        // console.log(`Movement Speed: ${getParam(e.$[1])}`);
-        break;
-      default:
-        break;
-    }
+    
     effectNameMap.set(
       // deps.skillName,
       e.$[0],
       `skillName: ${deps.skillName}, effect: ${e.$[0]}: ${effect({effectName: e.$[0], prop1: getParam(e.$[1]), prop2: getParam(e.$[2]), prop3: getParam(e.$[3])})} (${govno(e.$[0])}) ${getParam(e.$[1])} ${e.$[2]} ${e.$[3]} ${e.$[4]}`
     );
+    map.set(deps.skillName, {skillName: deps.skillName, effect: effect({effectName: e.$[0], prop1: getParam(e.$[1]), prop2: getParam(e.$[2]), prop3: getParam(e.$[3])}), operateCond: ""})
   });
 
   return [];
@@ -94,7 +90,7 @@ function getParam(
     const toStr = param.$.length === 1 ? param.$.join('') : param.$.length === 2 ? param.$.join(' or ') : param.$.join(', ') 
     return toStr !== "all" ? toStr : null;
   } else {
-    return `{govno: ${param}}`;
+    return `{gouno: ${param}}`;
   }
 }
 
@@ -126,10 +122,67 @@ function effect(params: {effectName: string, prop1:string|null, prop2:string|nul
     case "p_avoid":
       return chetko(`Evasion`, prop1, prop2, prop3)
     default: 
-      return params;
+      return `${params}`;
   }
 }
 
 function chetko(text: string, prop1:string|null, prop2:string|null, prop3:string|null ) {
   return `${prop2?.indexOf("-") !== -1 ? 'Reduces' : 'Increases'} ${text}${prop1 ? prop1 : ''} by ${prop2}${prop3 === "per" ? "%" : ""}`
+}
+
+
+function operateCondReader(deps: {
+  operateCond: SkillOperateCond;
+  skillName: string;
+}) {
+  deps.operateCond?.$.map((oc) => {
+  operateCondNameMap.set(
+      // deps.skillName,
+      deps.skillName+"_"+oc.$[0],
+      `skillName: ${deps.skillName}, operateCond: ${oc.$[0]}: ${operateCond({operateCondName: oc.$[0], prop1: ocGetParam(oc.$[1])})} ${oc.$[2]}`
+    );
+  const m = map.get(deps.skillName)
+  if (m) map.set(deps.skillName, {skillName: deps.skillName, effect: m.effect, operateCond: operateCond({operateCondName: oc.$[0], prop1: ocGetParam(oc.$[1])})})
+  })
+}
+
+function ocGetParam(
+  param:
+    | string
+    | number
+    | {
+        $: string[];
+      }
+    | {
+        $: {
+          $: any[];
+        }[];
+      }
+    | undefined
+): string {
+  if (typeof param === "string") {
+    return `${param}`;
+  }
+  if (typeof param === "number") {
+    return `${param}`;
+  }
+  if (typeof param === "object") {   
+    // console.log(param.$.length);
+    let toStr = param.$.length === 1 ? param.$.join('') : param.$.join(', ') 
+    toStr = toStr.replace(/,(?=[^,]+$)/, ', or')
+    
+    return toStr;
+  } else {
+    return `{gouno: ${param}}`;
+  }
+}
+
+function operateCond(params: {operateCondName: string, prop1:string}) {
+  const {operateCondName, prop1} = params
+  switch (operateCondName) {
+    case "equip_weapon":
+      return `Usable when a ${prop1}`
+    default: 
+      return `${params}`;
+  }
 }

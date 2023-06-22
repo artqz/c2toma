c1aac67ac697e8abf2841c88a34b7f82d33b96de
artqz <djoctuk@yandex.ru>
2023-06-22 13:42:32 +0500

    add new ench weap

--- import/enchantBonuses.ts
+++ import/enchantBonuses.ts
@@ -30,21 +30,48 @@ export function calcWeaponAtk2(deps: { level: number; item: Item }) {
   let pAtk = 0;
   let mAtk = 0;
   if (deps.item.type === "weapon") {
-    const bonus = watk(deps.item.crystalType);
+    const bonus = watk2(deps.item.crystalType);
     if (deps.level <= 3) {
-      pAtk = Math.floor(
-        bonus[0] * deps.level + (deps.item.physicalDamage ?? 0)
-      );
-      mAtk = Math.floor(bonus[1] * deps.level + (deps.item.magicalDamage ?? 0));
+      
+        if (isBow(deps.item)) {
+          pAtk = Math.floor(bonus.bow * deps.level + (deps.item.physicalDamage ?? 0))
+        }
+        else if (isOneHand(deps.item)) {
+          pAtk = Math.floor(bonus.oneHand * deps.level + (deps.item.physicalDamage ?? 0))
+        }
+        else if (isTwoHand(deps.item)) {
+          pAtk = Math.floor(bonus.twoHand * deps.level + (deps.item.physicalDamage ?? 0))
+        }
+        else {pAtk = deps.item.physicalDamage ?? 0}
+      mAtk = Math.floor(bonus.magic * deps.level + (deps.item.magicalDamage ?? 0));
     } else {
-      pAtk = Math.floor(
-        bonus[0] * 3 +
-          bonus[0] * 2 * (deps.level - 3) +
+      if (isBow(deps.item)) {
+          pAtk = Math.floor(
+        bonus.bow * 3 +
+          bonus.bow * 2 * (deps.level - 3) +
           (deps.item.physicalDamage ?? 0)
       );
+        }
+        else if (isOneHand(deps.item)) {
+          pAtk = Math.floor(
+        bonus.oneHand * 3 +
+          bonus.oneHand * 2 * (deps.level - 3) +
+          (deps.item.physicalDamage ?? 0)
+      );
+        }
+        else if (isTwoHand(deps.item)) {
+          pAtk = Math.floor(
+        bonus.twoHand * 3 +
+          bonus.twoHand * 2 * (deps.level - 3) +
+          (deps.item.physicalDamage ?? 0)
+      );
+        }
+        else {pAtk = deps.item.physicalDamage ?? 0}
+
+     
       mAtk = Math.floor(
-        bonus[1] * 3 +
-          bonus[1] * 2 * (deps.level - 3) +
+        bonus.magic * 3 +
+          bonus.magic * 2 * (deps.level - 3) +
           (deps.item.magicalDamage ?? 0)
       );
     }
@@ -193,5 +220,75 @@ function watk(type: Crystal) {
       return [5, 4];
     default:
       return [1, 1];
+  }
+}
+//When enchanted, 
+//the P. Atk. of C grade weapons such as one-handed swords and blunt weapons, daggers and spears increases by 3. 
+//The P. Atk of two-handed swords and blunt weapons, dualswords, and two-handed fist-fighting weapons increases by 4. 
+//The P. Atk. of bow weapons increases by 6. 
+//The M. Atk. of all weapons increases by 3. From +4, P. Atk. and M. Atk. of all weapons are doubled.
+
+//oneHand = one-handed swords and blunt weapons, daggers and spears
+//twoHand = two-handed swords and blunt weapons, dualswords, and two-handed fist-fighting weapons 
+
+function isOneHand(item: Item) {
+  if (item.slotBitType === "rhand" && item.weaponType === "sword") {
+    return true
+  }
+  if (item.slotBitType === "rhand" && item.weaponType === "blunt") {
+    return true
+  }
+  if (item.slotBitType === "rhand" && item.weaponType === "dagger") {
+    return true
+  }
+  if (item.slotBitType === "rhand" && item.weaponType === "etc") {
+    return true
+  }
+  if (item.weaponType === "pole") {
+    return true
+  }
+  return false
+}
+
+function isTwoHand(item: Item) {
+  if (item.slotBitType === "lrhand" && item.weaponType === "sword") {
+    return true
+  }
+  if (item.slotBitType === "lrhand" && item.weaponType === "blunt") {
+    return true
+  }
+  if (item.slotBitType === "lrhand" && item.weaponType === "blunt") {
+    return true
+  }
+  if (item.weaponType === "dual") {
+    return true
+  }
+  if (item.weaponType === "dualfist") {
+    return true
+  }
+  return false
+}
+
+function isBow(item: Item) {
+  if (item.weaponType === "bow") {
+    return true
+  }
+  return false
+}
+
+function watk2(type: Crystal) {
+  switch (type) {
+    case "d":
+      return {oneHand: 3, twoHand: 2, bow: 4, magic: 2};
+    case "c":
+      return {oneHand: 3, twoHand: 4, bow: 6, magic: 3};
+    case "b":
+      return {oneHand: 3, twoHand: 4, bow: 6, magic: 3};
+    case "a":
+      return {oneHand: 4, twoHand: 5, bow: 8, magic: 3};
+    case "s":
+      return {oneHand: 5, twoHand: 6, bow: 10, magic: 4};
+    default:
+      return {oneHand: 1, twoHand: 1, bow: 1, magic: 1};
   }
 }

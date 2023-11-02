32cfd7e9cf568ba52bd30a0ed33cf7df17a9677d
artqz <djoctuk@yandex.ru>
2023-11-02 15:59:23 +0500

    fix ai

--- import/с1/c5/ai.ts
+++ import/с1/c5/ai.ts
@@ -1,10 +1,13 @@
 import { AiProps } from "../../../datapack/c1/ai";
+import { AiEntryC4, loadAiDataC4 } from '../../../datapack/c4/aidata';
+import { loadNpcDataC4 } from '../../../datapack/c4/npcdata';
 import {
   AiObjData,
   loadAiGf,
   AiSellList,
   AiObj,
 } from "../../../datapack/gf/aidata";
+import { loadNpcDataGF } from '../../../datapack/gf/npcdata';
 import { Ai, Item, Npc, AiSellList as AiSL } from "../../../result/types";
 
 export function generaAiC5(deps: {
@@ -13,18 +16,75 @@ export function generaAiC5(deps: {
 }) {
   let aiMap = new Map<string, Ai>();
   const aiData = loadAiGf();
+  const aiDataC4 = new Map(loadAiDataC4().map(a=> [a.name, a]));
+  const npcByNameC4 = new Map(loadNpcDataC4().map(n => [n.$[2], n]))
+  const npcByNameGF = new Map(loadNpcDataGF().map(n => [n.$[2], n]))
 
   for (const npc of deps.npcs.values()) {
-    const list = applyAi(aiData, npc);
-    if (list) {
-      aiMap.set(npc.ai, list);
+    const npcC4 = npcByNameC4.get(npc.npcName)
+    if (npcC4) {
+      const list = getAiC4(aiDataC4, npc);
+      if (list) {
+        aiMap.set(npc.ai, list);
+      }
+    } else {
+      const npcGF = npcByNameGF.get(npc.npcName)
+      if (npcGF) {
+        const list = applyAi(aiData, npc);
+        if (list) {
+          aiMap.set(npc.ai, list);
+        }
+      }
     }
+    
   }
 
   aiMap = filterAi({ ...deps, aiMap });
 
   return aiMap;
 }
+
+function getAiC4(aiData: Map<string, AiEntryC4>, npc: Npc): Ai | undefined {
+  const data = aiData.get(npc.ai);
+  if (data) {
+    return {
+      name: npc.ai,
+      super: data.super,
+      sell_lists: data.sell_lists,
+    };
+  } else {
+    return undefined;
+  }
+}
+
+function filterSellList(deps: { sellLists: AiSL[]; items: Map<number, Item> }) {
+  const itemByName = new Map(
+    Array.from(deps.items.values()).map((x) => [
+      x.itemName.replace("'", "_"),
+      x,
+    ])
+  );
+
+  const lists = new Map<number, AiSL>();
+  for (const [i, list] of deps.sellLists.entries()) {
+    const newList: AiSL = [];
+    for (const x of list.values()) {
+      if (!excludedItems.has(x[0])) {
+        const item = itemByName.get(x[0]);
+        if (item) {
+          newList.push([item.itemName, x[1], x[2], x[3]]);
+        }
+      } else {
+        console.log(`------------ ${x[0]}`);
+      }
+    }
+    lists.set(i, newList);
+  }
+
+  return Array.from(lists.values());
+}
+
+const excludedItems = new Set([""]);
 
 function applyAi(aiData: AiObj, npc: Npc): Ai | undefined {
   const data: AiObjData | undefined = aiData[npc.ai];

eb8c07ce8157e6f1a11253ef11c8ca13fffbe7c8
artqz <djoctuk@yandex.ru>
2024-01-07 15:04:29 +0500

    fix skills c5

--- datapack/c5/skillgrp.ts
+++ datapack/c5/skillgrp.ts
@@ -5,9 +5,9 @@ import { z } from "zod";
 const operType = {
   0: "A1",
   1: "A2",
-  2: "A3",
+  2: "P",
   3: "T",
-}
+};
 
 type SkillGrpEntry = {
   id: number;
@@ -21,11 +21,11 @@ type SkillGrpEntry = {
   hitTime: number;
   isMagic: number;
   aniChar: string;
-  desc: string;	
-  extraEff: number;		
+  desc: string;
+  extraEff: number;
   isEnch: number;
-  enchSkillId: number;			
-  unk0: number;		
+  enchSkillId: number;
+  unk0: number;
   unk1: number;
 };
 
@@ -33,7 +33,7 @@ const SkillGrpItem = z.object({
   skill_id: z.string(),
   skill_level: z.string(),
   icon_name: z.string(),
-  //  
+  //
   oper_type: z.string(),
   hp_consume: z.string(),
   mp_consume: z.string(),
@@ -41,14 +41,14 @@ const SkillGrpItem = z.object({
   cast_style: z.string(),
   //
   hit_time: z.string(), //skill_hit_time
-  is_magic: z.string(),	
-  ani_char: z.string(),	
-  desc: z.string(),	
-  extra_eff: z.string(),		
-  is_ench: z.string(),		
-  ench_skill_id: z.string(),			
-  UNK_0: z.string(),		
-  UNK_1: z.string(),	
+  is_magic: z.string(),
+  ani_char: z.string(),
+  desc: z.string(),
+  extra_eff: z.string(),
+  is_ench: z.string(),
+  ench_skill_id: z.string(),
+  UNK_0: z.string(),
+  UNK_1: z.string(),
 });
 
 export function loadSkillGrpC5(): SkillGrpEntry[] {
@@ -56,30 +56,31 @@ export function loadSkillGrpC5(): SkillGrpEntry[] {
     Fs.readFileSync("datapack/c5/skillgrp.txt", "utf8"),
     { delimiter: "\t", relaxQuotes: true, columns: true, bom: true }
   );
-  
-  const skills = SkillGrpItem.array().parse(skillsRaw);  
 
+  const skills = SkillGrpItem.array().parse(skillsRaw);
+  const testMap = new Map();
   return skills
-  .filter(x => x.skill_id < "50000") // тома пидор (убираем дроп и спойл из скилзов)
-  .map((x) => {        
-    return {
-      id: parseInt(x.skill_id),
-      level: parseInt(x.skill_level),
-      icon: x.icon_name.replace("icon.", ""),
-      operType: operType[x.oper_type as "0" | "1" | "2" | "3"],
-      hpConsume: parseInt(x.hp_consume),
-      mpConsume: parseInt(x.mp_consume),
-      castRange: parseInt(x.cast_range),
-      castStyle: parseInt(x.cast_style),
-      hitTime: parseInt(x.hit_time),
-      isMagic: parseInt(x.is_magic),
-      aniChar: x.ani_char,
-      desc: x.desc,	
-      extraEff: parseInt(x.extra_eff),		
-      isEnch: parseInt(x.is_ench),		
-      enchSkillId: parseInt(x.ench_skill_id),			
-      unk0: parseInt(x.UNK_0),		
-      unk1: parseInt(x.UNK_1),
-    };   
-  });  
+    .filter((x) => x.skill_id < "50000") // тома пидор (убираем дроп и спойл из скилзов)
+    .map((x) => {
+      testMap.set(x.oper_type, parseInt(x.skill_id));
+      return {
+        id: parseInt(x.skill_id),
+        level: parseInt(x.skill_level),
+        icon: x.icon_name.replace("icon.", ""),
+        operType: operType[x.oper_type as "0" | "1" | "2" | "3"],
+        hpConsume: parseInt(x.hp_consume),
+        mpConsume: parseInt(x.mp_consume),
+        castRange: parseInt(x.cast_range),
+        castStyle: parseInt(x.cast_style),
+        hitTime: parseInt(x.hit_time),
+        isMagic: parseInt(x.is_magic),
+        aniChar: x.ani_char,
+        desc: x.desc,
+        extraEff: parseInt(x.extra_eff),
+        isEnch: parseInt(x.is_ench),
+        enchSkillId: parseInt(x.ench_skill_id),
+        unk0: parseInt(x.UNK_0),
+        unk1: parseInt(x.UNK_1),
+      };
+    });
 }
--- import/с1/c5/skills.ts
+++ import/с1/c5/skills.ts
@@ -1,16 +1,21 @@
-import { loadSkillDataC4 } from '../../../datapack/c4/skilldata';
-import { loadSkillGrpC5 } from '../../../datapack/c5/skillgrp';
-import { loadSkillDataGF } from '../../../datapack/gf/skilldata';
-import { SkillEntry } from '../../../datapack/types';
+import { loadSkillDataC4 } from "../../../datapack/c4/skilldata";
+import { loadSkillGrpC5 } from "../../../datapack/c5/skillgrp";
+import { loadSkillDataGF } from "../../../datapack/gf/skilldata";
+import { SkillEntry } from "../../../datapack/types";
 
-export function generateSkillDataC5 () {
-  const skillByIdC4 = new Map(loadSkillDataC4().map(s => [s.skill_id+"_"+s.level, s]))
-  const skillByIdGF = new Map(loadSkillDataGF().map(s => [s.skill_id+"_"+s.level, s]))
-  const skills: SkillEntry[] = []
+export function generateSkillDataC5() {
+  const skillByIdC4 = new Map(
+    loadSkillDataC4().map((s) => [s.skill_id + "_" + s.level, s])
+  );
+  const skillByIdGF = new Map(
+    loadSkillDataGF().map((s) => [s.skill_id + "_" + s.level, s])
+  );
+  const skills: SkillEntry[] = [];
 
   for (const skillGrp of loadSkillGrpC5()) {
-    const skillC4 = skillByIdC4.get(skillGrp.id+"_"+ skillGrp.level)
-    if (skillC4) {
+    const skillC4 = skillByIdC4.get(skillGrp.id + "_" + skillGrp.level);
+
+    if (skillC4 && !skillC4.skill_name.includes("s_not_used")) {
       skills.push({
         skill_id: skillGrp.id,
         level: skillGrp.level,
@@ -25,26 +30,25 @@ export function generateSkillDataC5 () {
         mp_consume1: 0,
         mp_consume2: skillGrp.mpConsume,
         operate_cond: skillC4.operate_cond,
-      })     
-    }
-    else {
-      const skillGF = skillByIdGF.get(skillGrp.id+"_"+ skillGrp.level)
+      });
+    } else {
+      const skillGF = skillByIdGF.get(skillGrp.id + "_" + skillGrp.level);
       if (skillGF) {
         skills.push({
-       skill_id: skillGrp.id,
-        level: skillGrp.level,
-        skill_name: skillGF.skill_name,
-        effect: skillGF.effect,
-        operate_type: skillGrp.operType,
-        abnormal_time: skillGF.abnormal_time,
-        cast_range: skillGrp.castRange,
-        debuff: skillGF.debuff,
-        effect_point: skillGF.effect_point,
-        hp_consume: skillGrp.hpConsume,
-        mp_consume1: 0,
-        mp_consume2: skillGrp.mpConsume,
-        operate_cond: skillGF.operate_cond,
-      })     
+          skill_id: skillGrp.id,
+          level: skillGrp.level,
+          skill_name: skillGF.skill_name,
+          effect: skillGF.effect,
+          operate_type: skillGrp.operType,
+          abnormal_time: skillGF.abnormal_time,
+          cast_range: skillGrp.castRange,
+          debuff: skillGF.debuff,
+          effect_point: skillGF.effect_point,
+          hp_consume: skillGrp.hpConsume,
+          mp_consume1: 0,
+          mp_consume2: skillGrp.mpConsume,
+          operate_cond: skillGF.operate_cond,
+        });
       }
     }
   }

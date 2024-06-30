d1e22971be7bccb5f6701bff312b184a00912164
artqz <djoctuk@yandex.ru>
2024-06-30 09:38:24 +0500

    .

--- generate_c1.ts
+++ generate_c1.ts
@@ -16,7 +16,7 @@ import { loadNpcBuffs } from "./import/c1/npcBuffs";
 import { loadSkillCard } from "./import/c1/skillCard";
 import { loadZones } from "./import/c1/zones";
 
-const chronicle: Chronicle = "c5";
+const chronicle: Chronicle = "il";
 
 function init() {
   createDir(`result/data/${chronicle}`);
